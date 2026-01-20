bfa0d2e3f065b115a81cb904d6fbb3a17cb67fc4
Joe Downs <joe@dwns.dev>
2026-01-20 13:49:27 -0500

    Use lerp to move ship

--- ship.gd
+++ ship.gd
@@ -2,6 +2,7 @@ extends StaticBody3D
 
 enum Direction {FORWARD, BACKWARD, UP, DOWN, LEFT, RIGHT}
 
+var destination := Vector3()
 var directions = []
 const MAX_DIRECTIONS = 3
 
@@ -66,22 +67,42 @@ func move_and_rotate(delta, direction: Array, hard=false, change=false, speed=0)
 	rotation += Vector3(roll, yaw, pitch)
 	return
 
+func _check_coord_distance(src: Vector3, dest: Vector3, margin: float = 5.0) -> bool:
+	# Check if two coords are within some margin of each other
+	if abs(src.distance_to(dest)) <= margin:
+		print("Close enough to the destination!")
+		return true
+	else:
+		return false
+
+func _rand_coord() -> int:
+	return randi() % 100
+
+func choose_point() -> Vector3:
+	# Choose some point away from the ship and navigate there
+	var dest := Vector3()
+	dest.x = _rand_coord()
+	dest.y = _rand_coord()
+	dest.z = _rand_coord()
+	return dest
+
 func _init():
 	print("Initializing ships...")
 	seed(12345)
-	# directions.append(Direction.FORWARD)
-	# directions.append(Direction.LEFT)
 	for i in range(MAX_DIRECTIONS):
 		directions.append(Direction.FORWARD)
 
 func _process(delta):
 	var change = false
+	#print("Current position: %s" % position)
+	if _check_coord_distance(position, destination):
+		destination = choose_point()
+		print("Desired Position: %s" % destination)
 	# Chance to change direction
 	if !(randi() % 250):
 		change = true
-		for i in range(MAX_DIRECTIONS):
-			directions[i] = randi() % len(Direction)
 		print("Changing directions")
-		print(directions)
-	move_and_rotate(delta, directions, false, change, 1)
+		destination = choose_point()
+	#move_and_rotate(delta, directions, false, change, 1)
+	position = position.lerp(destination, delta*2)
 	return
